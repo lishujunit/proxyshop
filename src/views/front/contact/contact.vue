9651aed840557fad622a4ca943d875b9b45e6725
lishujun <12324324@qq.com>
2024-06-19 17:33:03 +0800

    d

--- src/views/front/contact/contact.vue
+++ src/views/front/contact/contact.vue
@@ -1,91 +1,96 @@
 <template>
-    <section class="wrapper image-wrapper bg-image bg-overlay bg-overlay-400 text-white bg-img">
-      <div class="container pt-17 pb-20 pt-md-19 pb-md-21 text-center">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h1 class="display-1 mb-3 text-white">Get in Touch</h1>
-            <nav class="d-inline-block" aria-label="breadcrumb">
-              <ol class="breadcrumb text-white">
-                <li class="breadcrumb-item"><a href="/">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Contact</li>
-              </ol>
-            </nav>
-            <!-- /nav -->
-          </div>
-          <!-- /column -->
+  <section class="wrapper image-wrapper bg-image bg-overlay bg-overlay-400 text-white bg-img">
+    <div class="container pt-17 pb-20 pt-md-19 pb-md-21 text-center">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h1 class="display-1 mb-3 text-white">Get in Touch</h1>
+          <nav class="d-inline-block" aria-label="breadcrumb">
+            <ol class="breadcrumb text-white">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Contact</li>
+            </ol>
+          </nav>
+          <!-- /nav -->
         </div>
-        <!-- /.row -->
+        <!-- /column -->
       </div>
-      <!-- /.container -->
-    </section>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </section>
 
-    <section class="wrapper bg-light angled upper-end">
-      <div class="container pb-11">
-        <div class="row mb-14 mb-md-16">
-          <div class="col-xl-10 mx-auto mt-n19">
-            <div class="card">
-              <div class="row gx-0">
-                <div class="col-lg-6 align-self-stretch">
-                  <div class="map map-full rounded-top rounded-lg-start">
-                    <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=4112A%20Main%20St%20p60,%20Flushing,%20NY%2011355+(ProxyShop)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps systems</a></iframe></div>
-                  </div>
-                  <!-- /.map -->
+  <section class="wrapper bg-light angled upper-end">
+    <div class="container pb-11">
+      <div class="row mb-14 mb-md-16">
+        <div class="col-xl-10 mx-auto mt-n19">
+          <div class="card">
+            <div class="row gx-0">
+              <div class="col-lg-6 align-self-stretch">
+                <div class="map map-full rounded-top rounded-lg-start">
+                  <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no"
+                      marginheight="0" marginwidth="0"
+                      src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=4112A%20Main%20St%20p60,%20Flushing,%20NY%2011355+(ProxyShop)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a
+                        href="https://www.gps.ie/">gps systems</a></iframe></div>
                 </div>
-                <!--/column -->
-                <div class="col-lg-6">
-                  <div class="p-10 p-md-11 p-lg-14">
-                    <div class="d-flex flex-row pb-5">
-                        Need to contact us about something? You can use one the email addresses listed below.
-                        We will respond within 24 hours（All time zones 7*24 hours in work）
-                        Choose from our contact information below：
+                <!-- /.map -->
+              </div>
+              <!--/column -->
+              <div class="col-lg-6">
+                <div class="p-10 p-md-11 p-lg-14">
+                  <div class="d-flex flex-row pb-5">
+                    Need to contact us about something? You can use one the email addresses listed below.
+                    We will respond within 24 hours（All time zones 7*24 hours in work）
+                    Choose from our contact information below：
+                  </div>
+                  <div class="d-flex flex-row">
+                    <div>
+                      <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-file-upload"></i> </div>
                     </div>
-                    <div class="d-flex flex-row">
-                      <div>
-                        <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-file-upload"></i> </div>
-                      </div>
-                      <div class="align-self-start justify-content-start">
-                        <h5 class="mb-1">Telegram</h5>
-                        <!-- <p class="mb-0"><a class="link-body" href="https://t.me/tunaproxysupport" target="_blank">@tunaproxy</a></p> -->
-                        <p class="mb-3"><a class="link-body" href="https://t.me/proxyshopio" target="_blank">@proxyshop service</a></p>
-                      </div>
+                    <div class="align-self-start justify-content-start">
+                      <h5 class="mb-1">Telegram</h5>
+                      <p class="mb-0"><a class="link-body"
+                          href="https://t.me/proxyshopservice" target="_blank">@proxyshop service</a></p>
+                      <p class="mb-3"><a class="link-body"
+                          href="https://t.me/proxyshop_service" target="_blank">@proxyshop service group</a></p>
                     </div>
-                    <!--/div -->
-                    <div class="d-flex flex-row">
-                      <div>
-                        <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-phone-volume"></i> </div>
-                      </div>
-                      <div>
-                        <h5 class="mb-1">Phone</h5>
-                        <p>+1 4376670659</p>
-                      </div>
+                  </div>
+                  <!--/div -->
+                  <div class="d-flex flex-row">
+                    <div>
+                      <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-phone-volume"></i> </div>
                     </div>
-                    <!--/div -->
-                    <div class="d-flex flex-row">
-                      <div>
-                        <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-envelope"></i> </div>
-                      </div>
-                      <div>
-                        <h5 class="mb-1">E-mail</h5>
-                        <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-body">Sales@proxyshop.io</a></p>
-                        <p class="mb-0"><a href="mailto:help@sandbox.com" class="link-body">support@proxyshop.io</a></p>
-                      </div>
+                    <div>
+                      <h5 class="mb-1">Phone</h5>
+                      <p>+1 4376670659</p>
                     </div>
-                    <!--/div -->
+                  </div>
+                  <!--/div -->
+                  <div class="d-flex flex-row">
+                    <div>
+                      <div class="icon text-primary fs-28 me-4 mt-n1"> <i class="uil uil-envelope"></i> </div>
+                    </div>
+                    <div>
+                      <h5 class="mb-1">E-mail</h5>
+                      <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-body">Sales@proxyshop.io</a></p>
+                      <p class="mb-0"><a href="mailto:help@sandbox.com" class="link-body">support@proxyshop.io</a></p>
+                    </div>
                   </div>
                   <!--/div -->
                 </div>
-                <!--/column -->
+                <!--/div -->
               </div>
-              <!--/.row -->
+              <!--/column -->
             </div>
-            <!-- /.card -->
+            <!--/.row -->
           </div>
-          <!-- /column -->
+          <!-- /.card -->
         </div>
-        <!-- /.row -->
+        <!-- /column -->
       </div>
-      <!-- /.container -->
-    </section>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </section>
 </template>
 
 <style scoped>
